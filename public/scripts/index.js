82b65c813b44af64779956d5e773e61f5de5c774
Evlogi Aleksandrov <evlogi.aleksandrov@gmail.com>
2018-11-12 22:12:50 +0200

    Emit newMessage on createMessage

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,11 +1,6 @@
 const socket = io();
 socket.on('connect', () => {
 	console.log('Connected to server');
-
-	socket.emit('createMessage', {
-		to: 'Jen',
-		text: 'Hey,Jen.',
-	});
 });
 socket.on('disconnect', () => {
 	console.log('Disconnected from server');
--- server/server.js
+++ server/server.js
@@ -14,10 +14,9 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
 	console.log('New user connected');
 
-	socket.emit('newMessage', { from: 'Jon', text: 'blabla', createdAt: 123 });
-
 	socket.on('createMessage', message => {
 		console.log('createMessage', message);
+		io.emit('newMessage', { ...message, createdAt: new Date().getTime() });
 	});
 	socket.on('disconnect', () => {
 		console.log('Client disconnected');
