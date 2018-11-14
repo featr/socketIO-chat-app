8098a892166f7300eaf36623226749d10c0d1f24
Evlogi Aleksandrov <evlogi.aleksandrov@gmail.com>
2018-11-14 14:22:39 +0200

    Format timestamps using momentjs

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -7,15 +7,17 @@ socket.on('disconnect', () => {
 });
 
 socket.on('newMessage', message => {
+	const formattedTime = moment(message.createdAt).format('k:mm a');
 	const li = $('<li></li>');
-	li.text(`${message.from}: ${message.text}`);
+	li.text(`${message.from} ${formattedTime}: ${message.text}`);
 	$('#messages').append(li);
 });
 
 socket.on('newLocationMessage', message => {
+	const formattedTime = moment(message.createdAt).format('k:mm a');
 	const li = $('<li></li>');
 	const a = $('<a target="_blank">My current location</a>');
-	li.text(`${message.from}: `);
+	li.text(`${message.from} ${formattedTime}: `);
 	a.attr('href', message.url);
 	li.append(a);
 	$('#messages').append(li);
